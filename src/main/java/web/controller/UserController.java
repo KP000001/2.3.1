9b542cb7dfcee72157b6584adcd8157ea1c5e979
Gaziz <gazizbakhyt@gmail.com>
2021-11-30 08:46:48 +0600

    latest commit

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,11 +1,11 @@
 package web.controller;
 
-import web.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import web.model.User;
 import web.service.UserService;
 
 /**
@@ -28,7 +28,7 @@ public class UserController {
     }
 
     @GetMapping("/new")
-    public String addUser(Model model) {
+    public String addUser(ModelMap model) {
         model.addAttribute("user", new User());
         return "new-user";
     }
